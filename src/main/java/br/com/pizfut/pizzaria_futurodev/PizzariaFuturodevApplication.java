5e2e42bfdc3a50d40ff6198f515d66e016a2c6c4
Rafaelpfigueira <rafael_figueira@estudante.sesisenai.org.br>
2022-10-14 21:10:44 -0300

    inicial commit

---
+++ src/main/java/br/com/pizfut/pizzaria_futurodev/PizzariaFuturodevApplication.java
@@ -0,0 +1,13 @@
+package br.com.pizfut.pizzaria_futurodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaFuturodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaFuturodevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pizfut/pizzaria_futurodev/PizzariaFuturodevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pizfut.pizzaria_futurodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaFuturodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
